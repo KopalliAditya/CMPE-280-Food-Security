60008cbe8ea680873b10f4978d34791322270fdd
Aditya Kopalli <kopalliaditya96@gmail.com>
2023-11-12 01:14:53 -0800

    "Header"

--- src/components/header.js
+++ src/components/header.js
@@ -16,15 +16,19 @@ const Header = () => {
 
     return (
         <div className="container" style={containerStyle}>
-            <div className="row">
-                <div className="col-md-4">
-                    {/* Inline SVG Leaf Icon */}
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" style={{ width: '100px', height: '100px', fill: '#1976d2' }}>
-                        <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-1 18h2v-2h-2zm2-4h-2V8h2v8zm-3.1-10.11L9 9l1.41-1.41L12 9.17l1.59-1.59L15 9l-3.1 3.89z" />
-                    </svg>
-                </div>
+        <div className="row">
+            <div className="col-md-4">
+                {/* Inline SVG Leaf Icon */}
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" style={{ width: '100px', height: '100px', fill: '#1976d2' }}>
+                    <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-1 18h2v-2h-2zm2-4h-2V8h2v8zm-3.1-10.11L9 9l1.41-1.41L12 9.17l1.59-1.59L15 9l-3.1 3.89z" />
+                </svg>
+            </div>
+            <div className="col-md-8 d-flex align-items-center">
+                <h1 style={{ marginLeft: '18px', fontStyle: 'italic', color: '#1976d2' }}>Food Security Dashboard</h1>
             </div>
         </div>
+    </div>
+    
     );
 };
 
